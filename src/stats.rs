fd621c61df3719d9d37de2ea1404c35c369ffb53
Nicolas Graves <ngraves@ngraves.fr>
2023-06-02 09:34:31 +0200

    Deleting benchmarks.

--- src/stats.rs
+++ src/stats.rs
@@ -876,25 +876,3 @@ mod tests {
         assert_eq!([1e30f64, 1.2f64, -1e30f64].sum(), 1.2);
     }
 }
-
-#[cfg(test)]
-mod bench {
-    use Bencher;
-    use stats::Stats;
-
-    #[bench]
-    pub fn sum_three_items(b: &mut Bencher) {
-        b.iter(|| {
-            [1e20f64, 1.5f64, -1e20f64].sum();
-        })
-    }
-    #[bench]
-    pub fn sum_many_f64(b: &mut Bencher) {
-        let nums = [-1e30f64, 1e60, 1e30, 1.0, -1e60];
-        let v = (0..500).map(|i| nums[i % 5]).collect::<Vec<_>>();
-
-        b.iter(|| {
-            v.sum();
-        })
-    }
-}
